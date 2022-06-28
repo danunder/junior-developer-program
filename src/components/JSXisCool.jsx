a99a79ec1708909b0f498d4c2ae456bad5ec76fb
danunder <danieljrobertson@gmail.com>
2022-06-28 13:23:06 -0400

    update stats

--- src/components/JSXisCool.jsx
+++ src/components/JSXisCool.jsx
@@ -19,15 +19,15 @@ const JSXisCool = (props) => {
             <img className="smaller" src="/cool-cat.jpg" alt="JSX - so cool" />
           </div>
       )}
-      <button 
-        style={ {padding: '20px', margin: '20px', fontSize: '18px' } } 
+      <button
+        style={ {padding: '20px', margin: '20px', fontSize: '18px' } }
         onClick={handleClick}>
           {isCool ? "No, JSX is not cool" : "JSX is cool"}
       </button>
       {isCool === false && <p>Not everyone can be JSX</p>}
       {isCool === false && (<div><img alt="JSX not cool?" src="/grumpy-cat.jpg" />
         <p>But seriously though, JSX is a syntax extension to JavaScript, so yes it's very cool.</p>
-        <p>Fun fact, JavaScript is used by 95.8% of websites as a client-side programming language.</p>
+        <p>Fun fact, JavaScript is used by 98% of websites as a client-side programming language.</p>
         <p>By embedding HTML in Javascript, you are able to take advantage of everything that Javascript offers, directly in your HTML.</p>
         <p>Let's look under the hood...</p>
         <p><Link to="/single-page-applications" >Single Page Applications</Link></p></div>)
@@ -41,7 +41,7 @@ JSXisCool.propTypes = {
 };
 
 JSXisCool.defaultProps = {
-  isJSXCool: undefined 
+  isJSXCool: undefined
 };
 
 export default JSXisCool;
